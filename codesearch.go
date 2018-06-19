62a626f3f555343ab42abaeda0803a97320f5f65
Yousif Aldolaijan <aldolaij@usc.edu>
2018-06-18 21:02:40 -0700

    When link to file is clicked all the lines are numbered and page scrolls to location of line with the searched query and the line is highlighted

--- codesearch.go
+++ codesearch.go
@@ -3,12 +3,14 @@ package main
 import (
 	"codesearch/indexer"
 	"codesearch/util"
+	"fmt"
 	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -54,16 +56,30 @@ func RepoIndexHandler(w http.ResponseWriter, r *http.Request) {
 
 // FileHandler Handles Opening files for user
 func FileHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("url %s", r.URL)
-
-	fileName := r.URL.Query()["f"][0]
+	q := r.URL.Query()
+	fileName := q["f"][0]
+	lineStr := q["l"][0]
+	line, err := strconv.Atoi(lineStr)
+	util.CheckError(err)
+	content := q["c"][0]
 
 	log.Printf("filename %s", fileName)
 	file, err := os.Open(fileName)
 	util.CheckError(err)
 	defer file.Close()
 
-	io.Copy(w, file)
+	fileContentByte, err := ioutil.ReadFile(fileName)
+	util.CheckError(err)
+	fileContentStr := strings.Split(string(fileContentByte), "\n")
+	fileContentStr = append(fileContentStr, "</pre></html>")
+	fileContentStr[line-1] = "<span style=\"background-color: yellow;\">" + content + "</span>"
+	for i, cont := range fileContentStr {
+		fileContentStr[i] = fmt.Sprintf("<div id=\"%d\">%d. %s</div>", i+1, i+1, cont)
+	}
+	fileContentStr = append([]string{"<html><pre>"}, fileContentStr...)
+	out := strings.Join(fileContentStr, "\n")
+
+	io.Copy(w, strings.NewReader(out))
 }
 
 func main() {
