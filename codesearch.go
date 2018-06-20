4a3efab0bb6331f09715ebe3cc020c5e83fcc7a4
Yousif Aldolaijan <aldolaij@usc.edu>
2018-06-19 22:30:12 -0700

    working tree list that shows all files within a specific repo but isn't positioned correctly and repeats files that are not directories

--- codesearch.go
+++ codesearch.go
@@ -5,7 +5,6 @@ import (
 	"codesearch/util"
 	"fmt"
 	"html/template"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -18,6 +17,7 @@ const (
 	templateDir = "templates/"
 	searchHTML  = templateDir + "search.html"
 	resultHTML  = templateDir + "result.html"
+	fileHTML    = templateDir + "file.html"
 )
 
 // SearchHandler Handles search requests
@@ -54,16 +54,50 @@ func RepoIndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
+// FileTreeString returns a string representation of FileTree
+func FileTreeString(file string, depth int) string {
+	out := ""
+	fi, err := os.Stat(file)
+	util.CheckError(err)
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		files, err := ioutil.ReadDir(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			fileName := f.Name()
+			if fileName[0] == '.' || fileName[0] == '#' || fileName[0] == '~' || fileName[len(fileName)-1] == '~' {
+				continue
+			}
+			out += strings.Repeat("&nbsp;", depth) + file + "/" + f.Name() + "</br>"
+			out += FileTreeString(file+"/"+f.Name(), depth+1)
+		}
+	case mode.IsRegular():
+		out += strings.Repeat("&nbsp;", depth) +
+			"<a href=\"/file/?f=" + file + "\">" + file + "</a></br>"
+	}
+
+	return out
+}
+
 // FileHandler Handles Opening files for user
 func FileHandler(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
-	fileName := q["f"][0]
-	lineStr := q["l"][0]
-	line, err := strconv.Atoi(lineStr)
-	util.CheckError(err)
-	content := q["c"][0]
+	var fileName, lineStr string
+	line := -1
+	if len(q["f"]) != 0 {
+		fileName = q["f"][0]
+	}
+	if len(q["l"]) != 0 {
+		lineStr = q["l"][0]
+		l, err := strconv.Atoi(lineStr)
+		util.CheckError(err)
+		line = l
+	}
 
-	log.Printf("filename %s", fileName)
+	log.Printf("fileName: %s", fileName)
 	file, err := os.Open(fileName)
 	util.CheckError(err)
 	defer file.Close()
@@ -71,15 +105,31 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 	fileContentByte, err := ioutil.ReadFile(fileName)
 	util.CheckError(err)
 	fileContentStr := strings.Split(string(fileContentByte), "\n")
-	fileContentStr = append(fileContentStr, "</pre></html>")
-	fileContentStr[line-1] = "<span style=\"background-color: yellow;\">" + content + "</span>"
+	fileContentStr = append(fileContentStr, "</pre>")
+	if line != -1 {
+		fileContentStr[line-1] = "<span style=\"background-color: yellow;\">" + fileContentStr[line-1] + "</span>"
+	}
 	for i, cont := range fileContentStr {
 		fileContentStr[i] = fmt.Sprintf("<div id=\"%d\">%d. %s</div>", i+1, i+1, cont)
 	}
-	fileContentStr = append([]string{"<html><pre>"}, fileContentStr...)
+	fileContentStr = append([]string{"<pre>"}, fileContentStr...)
 	out := strings.Join(fileContentStr, "\n")
 
-	io.Copy(w, strings.NewReader(out))
+	splitFile := strings.Split(fileName, "/")
+	rootFile := splitFile[0] + "/" + splitFile[1]
+
+	t, err := template.ParseFiles(fileHTML)
+	util.CheckError(err)
+	outStruct := struct {
+		FileName    string
+		FileContent template.HTML
+		FileTree    template.HTML
+	}{
+		FileName:    fileName,
+		FileContent: template.HTML(out),
+		FileTree:    template.HTML("<par>" + FileTreeString(rootFile, 0) + "</par>"),
+	}
+	t.Execute(w, outStruct)
 }
 
 func main() {
